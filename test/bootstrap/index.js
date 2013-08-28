c162b5831d29836b611baa3494c509d6ee864224
Jake Luer <jake@alogicalparadox.com>
2013-08-28 21:35:12 +0300

    add tests (uncompleted)

--- test/bootstrap/index.js
+++ test/bootstrap/index.js
@@ -1,35 +1,11 @@
 /*!
- * Attach chai to global should
+ * Assert
  */
 
-global.chai = require('chai');
-global.should = global.chai.should();
-
-/*!
- * Chai Plugins
- */
-
-//global.chai.use(require('chai-spies'));
-//global.chai.use(require('chai-http'));
+global.assert = require('simple-assert');
 
 /*!
  * Import project
  */
 
 global.eql = require('../..');
-
-/*!
- * Helper to load internals for cov unit tests
- */
-
-function req (name) {
-  return process.env.eql_COV
-    ? require('../../lib-cov/eql/' + name)
-    : require('../../lib/eql/' + name);
-}
-
-/*!
- * Load unexposed modules for unit tests
- */
-
-global.__eql = {};
