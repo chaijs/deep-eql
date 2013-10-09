076fdb692ca70ce5b992f0c9f78d9bd8a083c31d
Miroslav Bajtos <miroslav@strongloop.com>
2013-10-09 09:51:43 +0200

    index,test: support browserify
    Move the conditional require from /index.js to /test/bootstrap/index.js.
    
    This allows browserify to process the module and makes the code more compliant
    with upcoming ES6 modules.

--- test/bootstrap/index.js
+++ test/bootstrap/index.js
@@ -8,4 +8,6 @@ global.assert = require('simple-assert');
  * Import project
  */
 
-global.eql = require('../..');
+global.eql = process.env.type_COV
+  ? require('../../lib-cov/eql')
+  : require('../../lib/eql');
