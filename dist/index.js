5dabfab8dba2b2b99ebcdd66a825ff21ff59da41
Kristján Oddsson <koddsson@gmail.com>
2021-10-18 15:57:23 +0100

    More work

--- dist/index.js
+++ dist/index.js
@@ -1,48 +1,38 @@
 const TypedArray = Object.getPrototypeOf(Int8Array);
-const map = new WeakMap();
+const cache = new WeakMap();
 function deepEqual(left, right, comparator = Object.is) {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u;
-    let result = comparator(left, right);
-    if (result) {
+    var _a;
+    const result = comparator(left, right);
+    if (result)
         return true;
-    }
     if (typeof left !== 'object' || left === null)
         return false;
     if (typeof right !== 'object' || right === null)
         return false;
-    if (!map.has(left)) {
-        map.set(left, new WeakMap());
-    }
-    if (!map.has(right)) {
-        map.set(right, new WeakMap());
-    }
-    const results = (_a = map.get(right)) === null || _a === void 0 ? void 0 : _a.get(left);
-    if (typeof results === 'boolean') {
-        console.log('returning early', results);
-        return results;
-    }
-    (_b = map.get(left)) === null || _b === void 0 ? void 0 : _b.set(right, true);
-    (_c = map.get(right)) === null || _c === void 0 ? void 0 : _c.set(left, true);
+    if (!cache.has(left))
+        cache.set(left, new WeakMap());
+    if (!cache.has(right))
+        cache.set(right, new WeakMap());
+    const cacheResult = (_a = cache.get(right).get(left)) !== null && _a !== void 0 ? _a : cache.get(left).get(right);
+    if (cacheResult != null)
+        return cacheResult;
+    cache.get(left).set(right, true);
     if (!comparator(Object.getPrototypeOf(left), Object.getPrototypeOf(right))) {
-        (_d = map.get(right)) === null || _d === void 0 ? void 0 : _d.set(left, false);
-        (_e = map.get(left)) === null || _e === void 0 ? void 0 : _e.set(right, false);
+        cache.get(left).set(right, false);
         return false;
     }
     if (left instanceof Promise || left instanceof Symbol || left instanceof Function || left instanceof WeakMap || left instanceof WeakSet) {
-        (_f = map.get(right)) === null || _f === void 0 ? void 0 : _f.set(left, false);
-        (_g = map.get(left)) === null || _g === void 0 ? void 0 : _g.set(right, false);
+        cache.get(left).set(right, false);
         return false;
     }
     if ((left instanceof String || left instanceof Number || left instanceof Boolean || left instanceof Date)) {
         const x = comparator(left.valueOf(), right.valueOf());
-        (_h = map.get(right)) === null || _h === void 0 ? void 0 : _h.set(left, x);
-        (_j = map.get(left)) === null || _j === void 0 ? void 0 : _j.set(right, x);
+        cache.get(left).set(right, x);
         return x;
     }
     if (left instanceof RegExp) {
         const x = comparator(left.toString(), right.toString());
-        (_k = map.get(right)) === null || _k === void 0 ? void 0 : _k.set(left, x);
-        (_l = map.get(left)) === null || _l === void 0 ? void 0 : _l.set(right, x);
+        cache.get(left).set(right, x);
         return x;
     }
     let leftEntries = null;
@@ -63,27 +53,27 @@ function deepEqual(left, right, comparator = Object.is) {
         leftEntries = left;
         rightEntries = right;
     }
-    if (!leftEntries && !(Symbol.iterator in left)) {
+    if (!leftEntries && Symbol.iterator in left) {
+        leftEntries = left;
+        rightEntries = right;
+    }
+    if (!leftEntries || !rightEntries) {
         leftEntries = Object.entries(left);
         rightEntries = Object.entries(right);
     }
-    const leftIter = (leftEntries || left)[Symbol.iterator]();
-    const rightIter = (rightEntries || right)[Symbol.iterator]();
-    let leftCount = 0;
-    let rightCount = 0;
+    const leftIter = leftEntries[Symbol.iterator]();
+    const rightIter = rightEntries[Symbol.iterator]();
     const leftMap = new Map();
     const rightMap = new Map();
     while (true) {
         const leftStep = leftIter.next();
         const rightStep = rightIter.next();
+        if (leftStep.done !== rightStep.done) {
+            cache.get(left).set(right, false);
+            return false;
+        }
         if (leftStep.done && rightStep.done) {
             break;
-        }
-        if (!leftStep.done) {
-            leftCount += 1;
-        }
-        if (!rightStep.done) {
-            rightCount += 1;
         }
         if (Array.isArray(leftStep.value) && leftStep.value.length === 2) {
             leftMap.set(leftStep.value[0], leftStep.value[1]);
@@ -92,31 +82,18 @@ function deepEqual(left, right, comparator = Object.is) {
         else {
             const result = deepEqual(leftStep.value, rightStep.value, comparator);
             if (!result) {
-                (_m = map.get(right)) === null || _m === void 0 ? void 0 : _m.set(left, false);
-                (_o = map.get(left)) === null || _o === void 0 ? void 0 : _o.set(right, false);
+                cache.get(left).set(right, false);
                 return false;
             }
         }
     }
-    if (leftCount !== rightCount) {
-        (_p = map.get(right)) === null || _p === void 0 ? void 0 : _p.set(left, false);
-        (_q = map.get(left)) === null || _q === void 0 ? void 0 : _q.set(right, false);
-        return false;
-    }
     for (const item of leftMap) {
-        console.log(left);
-        console.log(right);
-        console.log(item[0]);
-        console.log(item[1]);
-        console.log(rightMap.get(item[0]));
         if (!deepEqual(item[1], rightMap.get(item[0]), comparator)) {
-            (_r = map.get(right)) === null || _r === void 0 ? void 0 : _r.set(left, false);
-            (_s = map.get(left)) === null || _s === void 0 ? void 0 : _s.set(right, false);
+            cache.get(left).set(right, false);
             return false;
         }
     }
-    (_t = map.get(right)) === null || _t === void 0 ? void 0 : _t.set(left, true);
-    (_u = map.get(left)) === null || _u === void 0 ? void 0 : _u.set(right, true);
+    cache.get(left).set(right, true);
     return true;
 }
 export { deepEqual };
--- index.ts
+++ index.ts
@@ -1,175 +1,139 @@
 const TypedArray = Object.getPrototypeOf(Int8Array)
 
-const map = new WeakMap<object, WeakMap<object, boolean>>()
+const cache = new WeakMap<object, WeakMap<object, boolean>>()
 
 function deepEqual(left: unknown, right: unknown, comparator = Object.is): boolean {
-  // 1. Call comparator on the given objects to optimize for early positive
-  // equality returns
-  let result = comparator(left, right)
-  if (result) {
-    return true
-  }
+  // 1. Optimize for early positive equality returns
+  const result = comparator(left, right)
+  if (result) return true
 
-  // 2. If they are primitives, they have no keys to iterate on so we must
-  // return false as they didn't pass through step 1.
+  // 2. Primitives have no keys. Treat them as unequal given step 1.
   if (typeof left !== 'object' || left === null) return false
   if (typeof right !== 'object' || right === null) return false
-  
-  if (!map.has(left)) {
-    map.set(left, new WeakMap())
-  }
-  if (!map.has(right)) {
-    map.set(right, new WeakMap())
-  }
+ 
+  // Set up the cache chain
+  if (!cache.has(left)) cache.set(left, new WeakMap())
+  if (!cache.has(right)) cache.set(right, new WeakMap())
 
-  const results = map.get(right)?.get(left)
-  if (typeof results === 'boolean') {
-    return results
-  }
+  const cacheResult = cache.get(right)!.get(left) ?? cache.get(left)!.get(right)
+  if (cacheResult != null) return cacheResult
 
-  // Temporarily set the operands in the memoize object to prevent blowing the stack
-  map.get(left)?.set(right, true);
-  map.get(right)?.set(left, true);
+  // Populate the cache to prevent too much recursion with circular references.
+  cache.get(left)!.set(right, true);
 
   // 3. Consider mismatched classes to be unequal.
   if (!comparator(Object.getPrototypeOf(left), Object.getPrototypeOf(right))) {
-    map.get(right)?.set(left, false)
-    map.get(left)?.set(right, false)
+    cache.get(left)!.set(right, false)
     return false
   }
 
-  // 4. If they are non-iterable classes (Promise, Symbol, Function, WeakMap,
-  // WeakSet), they have no keys to iterate so we must return false as they
-  // didn't pass through step 1.
+  // 4. Consider uniterable classes opaque. Treat them as unequal given step 1.
   if (left instanceof Promise || left instanceof Symbol || left instanceof Function || left instanceof WeakMap || left instanceof WeakSet) {
-    map.get(right)?.set(left, false)
-    map.get(left)?.set(right, false)
+    cache.get(left)!.set(right, false)
     return false
   }
 
   // 5. "Unboxing" primitives.
-  
-  // 5.1. If they are "boxed primitives" (String, Number, Boolean & Date), we
-  // "unpack" them using `.valueOf`. Re-run the comparator check with the
-  // unpacked primitives.  See step 2.
+  // 5.1. Compare unpacked "boxed primitives" using `.valueOf`. See step 2.
   if ((left instanceof String || left instanceof Number || left instanceof Boolean || left instanceof Date)) {
     const x =  comparator(left.valueOf(), right.valueOf())
-    map.get(right)?.set(left, x)
-    map.get(left)?.set(right, x)
+    cache.get(left)!.set(right, x)
     return x
   }
- 
-  // 5.2. Edge Case: RegExp are similar to "boxed primitives" in that they have
-  // an underlying value which is access with `.toString` as opposed to
-  // `.valueOf`.
+
+  // 5.2. Compare unpacked RegExp using `.toString`. See step 2.
   if (left instanceof RegExp) {
     const x = comparator(left.toString(), right.toString())
-    map.get(right)?.set(left, x)
-    map.get(left)?.set(right, x)
+    cache.get(left)!.set(right, x)
     return x
   }
 
-
-  // 6. Iterating object entries.
+  // 6. "Unboxing" iterables
   let leftEntries: Iterable<unknown> | null = null
   let rightEntries: Iterable<unknown> | null = null
 
-  // 6.1. Edge Case: Errors has no enumerable keys so we need to emulate them.
-  // A error stacktrace will always be unique so we skip that property as well.
+  // 6.1. Construct entries from Errors as they have no enumerable keys.
   if (left instanceof Error) {
     leftEntries = [['name', left.name], ['message', left.message], ['code', (left as any).code]]
     rightEntries = [['name', (right as Error).name], ['message', (right as Error).message], ['code', (right as any).code]]
   }
   
-  // 6.2. Edge Case: DataView is not iterable, we can unpack the DataView into
-  // an ArrayBuffer and let the ArrayBuffers drop down into step 6.4 to be
-  // processed further.
+  // 6.2. Construct entries from DataViews as they are uniterable.
   if (left instanceof DataView) {
     leftEntries = new Uint8Array(left.buffer)
     rightEntries = new Uint8Array((right as DataView).buffer)
   }
   
-  // 6.3. Edge Case: ArrayBuffer is not iterable since it doesn't have a width.
-  // We can assume a width to make it iterable. We assume a width of 8 since
-  // that's the smallest typed array there is. By packing the ArrayBuffer into
-  // an Uint8Array we make it iterable and can continue on. The size of the
-  // typed array doesn't concern us since we are simply interested in the bytes
-  // to byte comparison.
+  // 6.3. Construct entries from ArrayBuffer as they are uniterable.
   if (left instanceof ArrayBuffer) {
     leftEntries = new Uint8Array(left)
     rightEntries = new Uint8Array(right as ArrayBuffer)
   }
   
-  // 6.4. Performance: Arrays are already iterable so there is no need to unpack them.
+  // 6.4. Arrays are already iterable so there is no need to unbox them.
   if(Array.isArray(left) || left instanceof TypedArray) {
     leftEntries = left as Array<unknown>
     rightEntries = right as Array<unknown>
   }
+
+  // 6.5. Generic iterables.
+  if (!leftEntries && Symbol.iterator in left) {
+    leftEntries = left as Iterable<unknown>
+    rightEntries = right as Iterable<unknown>
+  }
   
-  // 6.5. Capture everything that isn't iterable and we haven't already
-  // processed.
-  if (!leftEntries && !(Symbol.iterator in (left as any))) {
+  // 6.6. Default case.
+  if (!leftEntries || !rightEntries) {
     leftEntries = Object.entries((left as object))
     rightEntries = Object.entries((right as object))
   }
 
-  const leftIter = (leftEntries || (left as Iterable<unknown>))[Symbol.iterator]()
-  const rightIter = (rightEntries || (right as Iterable<unknown>))[Symbol.iterator]()
-
-  let leftCount = 0
-  let rightCount = 0
-
+  const leftIter = leftEntries[Symbol.iterator]()
+  const rightIter = rightEntries[Symbol.iterator]()
   const leftMap = new Map()
   const rightMap = new Map()
+
+  // TODO: Add a test for this.
+  let isEntries = true
+  let iterResult = null
 
   while (true) {
     const leftStep = leftIter.next()
     const rightStep = rightIter.next()
+    
+    if (leftStep.done !== rightStep.done) {
+      cache.get(left)!.set(right, false)
+      return false
+    }
 
     if (leftStep.done && rightStep.done) {
       break
     }
 
-    if (!leftStep.done) {
-      leftCount += 1
-    }
-    if (!rightStep.done) {
-      rightCount += 1
-    }
-  
+    isEntries = isEntries && Array.isArray(leftStep.value) && leftStep.value.length === 2
 
     // Probably entry result
-    if (Array.isArray(leftStep.value) && leftStep.value.length === 2) {
+    if (isEntries) {
       leftMap.set(leftStep.value[0], leftStep.value[1]) 
       rightMap.set(rightStep.value[0], rightStep.value[1]) 
-    } else {
-      const result = deepEqual(leftStep.value, rightStep.value, comparator)
-      if (!result) {
-        map.get(right as object)?.set(left as object, false)
-        map.get(left as object)?.set(right as object, false)
-        return false 
-      }
+    }
+
+    iterResult = iterResult && deepEqual(leftStep.value, rightStep.value, comparator)
+    if (!isEntries && !iterResult) {
+      cache.get(left)!.set(right, false)
+      return false 
     }
   }
-  
-  // 7. If we didn't iterate over any keys, return false.
-  if (leftCount !== rightCount) {
-    map.get(right as object)?.set(left as object, false)
-    map.get(left as object)?.set(right as object, false)
-    return false
-  }
 
-  // 8. TODO
+  // 8. Iterate over the constructed map to compare entries.
   for (const item of leftMap) {
     if (!deepEqual(item[1], rightMap.get(item[0]), comparator)) {
-      map.get(right as object)?.set(left as object, false)
-      map.get(left as object)?.set(right as object, false)
+      cache.get(left)!.set(right, false)
       return false
     }
   }
 
-  map.get(right)?.set(left, true)
-  map.get(left)?.set(right, true)
+  cache.get(left)!.set(right, true)
   return true
 }
 
