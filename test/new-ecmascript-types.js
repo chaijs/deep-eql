bf2bb4d52c88e4ee268b9e87e68f842ded5cd4c3
Jordan Harband <ljharb@gmail.com>
2024-06-03 01:01:58 -0700

    fix: catch fake collections throwing (#100)
    Fixes #99

--- test/new-ecmascript-types.js
+++ test/new-ecmascript-types.js
@@ -97,6 +97,14 @@ describe('ES2015 Specific', function () {
       assert(eql(mapA, mapB), 'eql(Map { a => 1, b => 2, c => 3 }, Map { a => 1, b => 2, c => 3 })');
     });
 
+    (setExists ? it : it.skip)('returns false for fake Maps', function () {
+      var maplikeSet = new Set();
+      Object.defineProperty(maplikeSet, 'constructor', { enumerable: false, value: Map });
+      Object.setPrototypeOf(maplikeSet, Map.prototype);
+
+      assert(eql(maplikeSet, new Map()) === false, 'eql(Set pretending to be a Map, Map { })');
+    });
+
   });
 
   describeIf(symbolAndMapExist && typeof Map.prototype[Symbol.iterator] === 'function')('map iterator', function () {
@@ -238,6 +246,14 @@ describe('ES2015 Specific', function () {
       setA.add(setB);
       setB.add(setA);
       assert(eql(setA, setB) === true, 'eql(Set { -> }, Set { <- }) === true');
+    });
+
+    (mapExists ? it : it.skip)('returns false for fake Sets', function () {
+      var setlikeMap = new Map();
+      Object.defineProperty(setlikeMap, 'constructor', { enumerable: false, value: Set });
+      Object.setPrototypeOf(setlikeMap, Set.prototype);
+
+      assert(eql(setlikeMap, new Set()) === false, 'eql(Map pretending to be a Set, Set { })');
     });
 
   });
